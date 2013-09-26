ebb9947d79f47210f6a30a26b383b13f2df2b684
Torjus <torjussv@stud.ntnu.no>
2013-09-26 15:42:41 +0200

    First commit

---
+++ CMakeFiles/UAVNAV.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/UAVNAV.dir/main.cpp.o"
+  "UAVNAV.pdb"
+  "UAVNAV"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/UAVNAV.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
